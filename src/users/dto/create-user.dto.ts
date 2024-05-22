c2b6ee88f67817bd3ef46f1ad9cd58f09d8ca73f
Tanat Arora <tanat.navin@gmail.com>
2024-05-22 15:19:03 +0700

    finished and deployed basic crud app
    finished and deployed basic crud app

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,15 @@
+import { IsEmail, IsEnum, IsString, IsNotEmpty} from 'class-validator';
+
+export class CreateUserDto {
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @IsEmail()
+    email: string;
+
+    @IsEnum(['INTERN', 'ENGINEER', 'ADMIN'], {
+        message: 'Role must be one of the following values: INTERN, ENGINEER, ADMIN'
+    })
+    role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+}
